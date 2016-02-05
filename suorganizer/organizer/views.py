47995a654bd53d100837a12c2922231748698dab
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-02-05 05:55:20 +0100

    started url config

--- suorganizer/organizer/views.py
+++ suorganizer/organizer/views.py
@@ -1,12 +1,18 @@
-from django.http.response import HttpResponse
-from django.template import Context, loader
+from django.shortcuts import get_object_or_404, render
+
 
 from .models import Tag
 
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    template = loader.get_template('organizer/tag_list.html')
-    context = Context({'tag_list': tag_list})
-    output = template.render(context)
-    return HttpResponse(output)
+    return render(
+        request, 'organizer/tag_list.html', {'tag_list': tag_list}
+    )
+
+
+def tag_detail(request, slug):
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render(
+        request, 'organizer/tag_detail.html', {'tag': tag}
+    )
