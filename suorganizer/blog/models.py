3e3a005e9236861693060ea43c3397b5f5f421fb
Rodrigo Villatoro <rodrigovillatoro@hotmail.com>
2016-02-05 18:41:02 +0100

    get_absolute_urls on templates

--- suorganizer/blog/models.py
+++ suorganizer/blog/models.py
@@ -1,25 +1,28 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 from organizer.models import Startup, Tag
 
 
 class Post(models.Model):
     title = models.CharField(max_length=63)
-    slug = models.SlugField(
-        max_length=63, help_text='A label for URL conf.',
-        unique_for_month='pub_date')
+    slug = models.SlugField(max_length=63, help_text='A label for URL conf.',
+                            unique_for_month='pub_date')
     text = models.TextField()
-    pub_date = models.DateField(
-        'date published',auto_now_add=True)
-    tags = models.ManyToManyField(
-        Tag, related_name='blog_posts')
-    startups = models.ManyToManyField(
-        Startup, related_name='blog_posts')
+    pub_date = models.DateField('date published',auto_now_add=True)
+    tags = models.ManyToManyField(Tag, related_name='blog_posts')
+    startups = models.ManyToManyField(Startup, related_name='blog_posts')
 
     def __str__(self):
         return '{} on {}'.format(
-            self.title, self.pub_date.strftime('%Y-%m-%d')
-        )
+            self.title, self.pub_date.strftime('%Y-%m-%d'))
+
+    def get_absolute_url(self):
+        return reverse('blog_post_detail', kwargs={
+            'year': self.pub_date.year,
+            'month': self.pub_date.month,
+            'slug': self.slug,
+        })
 
     class Meta:
         verbose_name = 'blog post'
